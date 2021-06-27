e6f532f921bd87443a392da2e90d9abd9177d356
Shabab Rizvi <78068691+shababrizvicse@users.noreply.github.com>
2021-06-28 00:58:20 +0530

    Add files via upload

---
+++ alexa.py
@@ -0,0 +1,50 @@
+
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print("Listening.....")
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'alexa' in command:
+                command = command.replace('alexa', '')
+                print(command)
+
+    except:
+        pass
+    return command
+def run_alexa():
+    command = take_command()
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('ok wait playing' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('current time is' + time)
+    elif 'tell me about' in command:
+        person = command.replace('tell me about', '')
+        info = wikipedia.summary(person,3)
+        talk(info)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('sorry i cannot understand you this time')
+        
+while True:
+    run_alexa()
